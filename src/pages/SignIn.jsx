d3a6aa3929f2eede25d963115ee8cdb05185454f
Mohammed Hussain <mohammedhussain0898@gmail.com>
2024-06-20 21:05:37 +0530

    add the edit functionality to the profile page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { FaEyeSlash,FaEye  } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import Oauth from '../components/Oauth';
